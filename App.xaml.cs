90425dea3b25481e08e8d099be51b93a4262a514
smokywastaken <123833169+smokywastaken@users.noreply.github.com>
2024-06-24 14:51:29 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace Lockbox
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+            /*
+             * if (flag):
+             *      LockPage = new AppShell();
+             * else 
+             *      SetupView = new AppShell();
+             */
+        }
+    }
+}
---
+++ SetupView.xaml.cs
@@ -0,0 +1,17 @@
+using Microsoft.Maui;
+
+namespace Lockbox;
+
+public partial class SetupView : ContentPage
+{
+	public SetupView()
+	{
+		InitializeComponent();
+	}
+    private void OnSubmitClicked(object sender, EventArgs e)
+    {
+        string inputText = masterpass.Text;
+        
+    }
+
+}
